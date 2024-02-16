18a0d2210d439c50bac763ee38a66437f514c188
Midgar <80575182+JesseCaddell@users.noreply.github.com>
2024-02-15 20:43:51 -0800

    remix without optional

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,23 @@
-import type { MetaFunction } from "@remix-run/node";
+import React from 'react';
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+const HomePage = () => {
+  const blogPosts = [
+    {id: 1, title: 'Introduction to Remix', slug: 'introduction-to-remix' },
+    {id: 2, title: 'Getting Started with Remix', slug: 'getting-started-with-remix' }
   ];
-};
 
-export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div>
+      <h1>Welcome to my blog!</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {blogPosts.map((post) => (
+          <li key={post.id}>
+            <a href={`/posts/${post.slug}`}>{post.title}</a>
+          </li>
+        ))}
       </ul>
     </div>
   );
-}
+};
+
+export default HomePage;
